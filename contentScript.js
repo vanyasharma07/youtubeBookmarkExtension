27c662018cb978d95bff2ddb3c9142f59cf54d0c
Vanya Sharma <vanya2007sharma@gmail.com>
2025-08-11 10:45:11 +0530

    add message listener to detect and handle new videos

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,13 @@
 (() =>{
      let youtubeLeftControls, youtubePlayer;
+     let currentVideo = "";
+
+     chrome.runtime.onMessage.addListner((obj, sender, response) => {
+        const {type, value, videoId} = obj;
+
+        if(type === "NEW"){
+            currentVideo = videoId;
+            newVideoLoaded();
+        }
+     })
 })();
