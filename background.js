fbb9c8027d4b149f9225e230afd3d9308c60f64a
Vanya Sharma <vanya2007sharma@gmail.com>
2025-08-11 10:38:43 +0530

    Extract video ID from YouTube tab URL

--- background.js
+++ background.js
@@ -1,12 +1,11 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+chrome.tabs.onUpdate.addListener((tabId, tab) =>{c
+  if(tab.url && tab.url.includes("youtube.com/watch")){
+    const queryParameters = tab.url.split("?")[1];//? why [1]
+    const urlParameters = new URLSearchParams(queryParameters);//?
+
+    chrome.tabs.sendMessgae(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v")//
+    })
+  }
+})
