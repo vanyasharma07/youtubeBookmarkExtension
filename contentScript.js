e1ec679e632084c20f48328e907cc0583909591f
Vanya Sharma <vanya2007sharma@gmail.com>
2025-08-11 10:57:33 +0530

    adding and checkimg bookmark-btn

--- contentScript.js
+++ contentScript.js
@@ -9,5 +9,17 @@
             currentVideo = videoId;
             newVideoLoaded();
         }
-     })
+     });
+
+     const newVideoLoaded = () =>{
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+
+        if (!bookmarkBtnExists) {
+            const bookmarkBtn = document.createElement("img");
+
+            bookmarkBtn.src = chrome.runtime.getURL("assests/bookmark.png");
+            bookmarkBtn.className = "ytp-button" + "bookmark-btn";
+            bookmarkBtn.title = "Click to bookmark current timestamp";
+        }
+     }
 })();
